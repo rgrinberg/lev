857931ec364bf9e62ccdb8566922554d47876318
Rudi Grinberg <me@rgrinberg.com>
2021-11-30 13:50:45 -0600

    feature: bip buffers
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

---
+++ lev-fiber/src/util/bip_buffer.ml
@@ -0,0 +1,82 @@
+open Stdune
+
+module type Array = sig
+  type t
+
+  type elt
+
+  val length : t -> int
+end
+
+module Slice = struct
+  type t = { pos : int; len : int }
+end
+
+module Make (Array : Array) = struct
+  type t = {
+    buf : Array.t;
+    mutable a_start : int;
+    mutable a_end : int;
+    mutable b_end : int;
+    mutable b_inuse : bool;
+    mutable reserving : bool;
+  }
+
+  let buffer t = t.buf
+
+  let _invariant t =
+    if t.b_inuse then
+      assert (t.b_end - t.a_start > Array.length t.buf - t.a_end)
+
+  let create buf =
+    {
+      buf;
+      a_start = 0;
+      a_end = 0;
+      b_end = 0;
+      b_inuse = false;
+      reserving = false;
+    }
+
+  let peek t =
+    if t.a_start < t.a_end then Some { Slice.pos = t.a_start; len = t.a_end }
+    else None
+
+  let is_empty t = t.a_start = t.a_end
+
+  let length t = t.a_end - t.a_start + t.b_end
+
+  let junk t size =
+    assert (length t >= size);
+    let read_len = min size (t.a_end - t.a_start) in
+    t.a_start <- t.a_start + read_len;
+    let size = read_len - size in
+    if size >= 0 then (
+      t.b_inuse <- false;
+      t.a_start <- 0;
+      t.a_end <- t.b_end;
+      t.b_end <- 0)
+
+  let unused t =
+    if t.b_inuse then t.a_start - t.b_end else Array.length t.buf - t.a_end
+
+  let write_start t = if t.b_inuse then t.b_end else t.a_end
+
+  let set_current_buffer t =
+    let space_left_for_a = Array.length t.buf - t.a_end in
+    let space_left_for_b = t.a_start - t.b_end in
+    if space_left_for_a < space_left_for_b then t.b_inuse <- true
+
+  let reserve t size =
+    if t.reserving then Code_error.raise "previous reserve not committed" [];
+    if unused t < size then None
+    else (
+      t.reserving <- true;
+      Some (write_start t))
+
+  let commit t ~len =
+    assert t.reserving;
+    if t.b_inuse then t.b_end <- t.b_end + len else t.a_end <- t.a_end + len;
+    t.reserving <- false;
+    set_current_buffer t
+end
---
+++ lev-fiber/test/bip_buffer_tests.ml
@@ -0,0 +1,56 @@
+open Stdune
+module Bytes = BytesLabels
+
+module B = Util.Bip_buffer.Make (struct
+  type elt = char
+
+  include Bytes
+end)
+
+let%expect_test "create" =
+  let len = 128 in
+  let b = B.create (Bytes.create len) in
+  let unused = B.unused b in
+  printfn "%d = %d" unused len;
+  assert (unused = len);
+  [%expect {| 128 = 128 |}]
+
+let%expect_test "is_empty" =
+  assert (B.is_empty (B.create (Bytes.create 100)));
+  [%expect {||}]
+
+let%expect_test "read empty" =
+  let b = B.create (Bytes.create 0) in
+  assert (B.peek b = None);
+  [%expect {||}]
+
+let%expect_test "bip buffers" =
+  let buf_size = 100 in
+  let b = B.create (Bytes.create buf_size) in
+  assert (B.is_empty b);
+  printfn "Unused space: %d" (B.unused b);
+  [%expect {| Unused space: 100 |}];
+  let mystr = "Test Foo Bar" in
+  let mystr_len = String.length mystr in
+  let dst_pos = Option.value_exn (B.reserve b mystr_len) in
+  let buf = B.buffer b in
+  Bytes.blit_string ~dst:buf ~dst_pos ~src:mystr ~src_pos:0 ~len:mystr_len;
+  B.commit b ~len:mystr_len;
+  assert (B.length b = mystr_len);
+  let unused = B.unused b in
+  let expected = buf_size - mystr_len in
+  printfn "unused %d. Expected %d" unused expected;
+  assert (unused = expected);
+  [%expect {| unused 88. Expected 88 |}];
+  (* Now we try to read 4 characters *)
+  let () =
+    let slice = Option.value_exn (B.peek b) in
+    let read_len = 4 in
+    assert (slice.len >= read_len);
+    let read = Bytes.create read_len in
+    Bytes.blit ~src:(B.buffer b) ~src_pos:slice.pos ~dst:read ~dst_pos:0
+      ~len:read_len;
+    printfn "Read: %s" (Bytes.to_string read);
+    B.junk b read_len
+  in
+  [%expect {| Read: Test |}]
