0b4a758355bb58f8874d0d5b3d728cfb724beb53
Rudi Grinberg <me@rgrinberg.com>
2021-11-30 16:11:56 -0600

    fix: more buffer bugs
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lev-fiber/src/util/bip_buffer.ml
+++ lev-fiber/src/util/bip_buffer.ml
@@ -1,5 +1,10 @@
 open Stdune
 
+module Blit = struct
+  type ('src, 'dst) t =
+    src:'src -> src_pos:int -> dst:'dst -> dst_pos:int -> len:int -> unit
+end
+
 module Slice = struct
   type t = { pos : int; len : int }
 end
@@ -31,7 +36,8 @@ let create buf ~len =
   }
 
 let peek t =
-  if t.a_start < t.a_end then Some { Slice.pos = t.a_start; len = t.a_end }
+  if t.a_start < t.a_end then
+    Some { Slice.pos = t.a_start; len = t.a_end - t.a_start }
   else None
 
 let is_empty t = t.a_start = t.a_end
@@ -42,31 +48,44 @@ let junk t size =
   assert (length t >= size);
   let read_len = min size (t.a_end - t.a_start) in
   t.a_start <- t.a_start + read_len;
-  let size = read_len - size in
-  if size >= 0 then (
-    t.b_inuse <- false;
-    t.a_start <- 0;
+  if t.a_start = t.a_end then (
+    t.a_start <- size - read_len;
     t.a_end <- t.b_end;
-    t.b_end <- 0)
+    t.b_end <- 0;
+    t.b_inuse <- false)
 
-let unused t = if t.b_inuse then t.a_start - t.b_end else t.buf_len - t.a_end
+let space_left_for_a t = t.buf_len - t.a_end
 
-let write_start t = if t.b_inuse then t.b_end else t.a_end
-
-let set_current_buffer t =
-  let space_left_for_a = t.buf_len - t.a_end in
-  let space_left_for_b = t.a_start - t.b_end in
-  if space_left_for_a < space_left_for_b then t.b_inuse <- true
+let space_left_for_b t = t.a_start - t.b_end
 
 let reserve t size =
   if t.reserving then Code_error.raise "previous reserve not committed" [];
-  if unused t < size then None
-  else (
+  let space_left_for_a = space_left_for_a t in
+  let space_left_for_b = space_left_for_b t in
+  if t.b_inuse then
+    if space_left_for_b >= size then (
+      t.reserving <- true;
+      Some t.b_end)
+    else None
+  else if space_left_for_a >= size then (
     t.reserving <- true;
-    Some (write_start t))
+    Some t.a_end)
+  else if space_left_for_b >= size then (
+    t.reserving <- true;
+    t.b_inuse <- true;
+    Some t.b_end)
+  else None
 
 let commit t ~len =
   assert t.reserving;
-  if t.b_inuse then t.b_end <- t.b_end + len else t.a_end <- t.a_end + len;
-  t.reserving <- false;
-  set_current_buffer t
+  if t.b_inuse then (
+    assert (t.b_end + len < t.a_start);
+    t.b_end <- t.b_end + len)
+  else (
+    assert (t.a_end + len < t.buf_len);
+    t.a_end <- t.a_end + len);
+  t.reserving <- false
+
+let compress _ _ = `Failure
+
+let resize _ _ _ ~size:_ = assert false
--- lev-fiber/test/bip_buffer_tests.ml
+++ lev-fiber/test/bip_buffer_tests.ml
@@ -2,14 +2,6 @@ open Stdune
 module Bytes = BytesLabels
 module B = Util.Bip_buffer
 
-let%expect_test "create" =
-  let len = 128 in
-  let b = B.create (Bytes.create len) ~len in
-  let unused = B.unused b in
-  printfn "%d = %d" unused len;
-  assert (unused = len);
-  [%expect {| 128 = 128 |}]
-
 let%expect_test "is_empty" =
   let len = 100 in
   assert (B.is_empty (B.create (Bytes.create len) ~len));
@@ -20,33 +12,55 @@ let%expect_test "read empty" =
   assert (B.peek b = None);
   [%expect {||}]
 
+let peek_str b ~len =
+  match B.peek b with
+  | None -> assert false
+  | Some s ->
+      assert (s.len >= len);
+      printfn "Requested %d. Available %d" len s.len;
+      let get len =
+        let dst = Bytes.create len in
+        let src = B.buffer b in
+        Bytes.blit ~dst ~dst_pos:0 ~src ~src_pos:s.pos ~len;
+        Bytes.to_string dst
+      in
+      let peek = get len in
+      if len = s.len then printfn "Peek: %S" peek
+      else printfn "Peek: %S (full: %S)" peek (get s.len);
+      peek
+
+let write_str b src =
+  let len = String.length src in
+  match B.reserve b len with
+  | None -> assert false
+  | Some dst_pos ->
+      let dst = B.buffer b in
+      Bytes.blit_string ~dst ~dst_pos ~src ~src_pos:0 ~len;
+      B.commit b ~len
+
 let%expect_test "bip buffers" =
-  let buf_size = 100 in
+  let buf_size = 16 in
   let b = B.create (Bytes.create buf_size) ~len:buf_size in
   assert (B.is_empty b);
-  printfn "Unused space: %d" (B.unused b);
-  [%expect {| Unused space: 100 |}];
-  let mystr = "Test Foo Bar" in
-  let mystr_len = String.length mystr in
-  let dst_pos = Option.value_exn (B.reserve b mystr_len) in
-  let buf = B.buffer b in
-  Bytes.blit_string ~dst:buf ~dst_pos ~src:mystr ~src_pos:0 ~len:mystr_len;
-  B.commit b ~len:mystr_len;
-  assert (B.length b = mystr_len);
-  let unused = B.unused b in
-  let expected = buf_size - mystr_len in
-  printfn "unused %d. Expected %d" unused expected;
-  assert (unused = expected);
-  [%expect {| unused 88. Expected 88 |}];
+  [%expect {| |}];
+  let () =
+    let mystr = "Test Foo|Bar" in
+    let mystr_len = String.length mystr in
+    write_str b mystr;
+    assert (B.length b = mystr_len)
+  in
+  [%expect {| |}];
   (* Now we try to read 4 characters *)
   let () =
-    let slice = Option.value_exn (B.peek b) in
-    let read_len = 4 in
-    assert (slice.len >= read_len);
-    let read = Bytes.create read_len in
-    Bytes.blit ~src:(B.buffer b) ~src_pos:slice.pos ~dst:read ~dst_pos:0
-      ~len:read_len;
-    printfn "Read: %s" (Bytes.to_string read);
+    let read_len = 8 in
+    let (_ : string) = peek_str b ~len:read_len in
     B.junk b read_len
   in
-  [%expect {| Read: Test |}]
+  [%expect
+    {|
+    Requested 8. Available 12
+    Peek: "Test Foo" (full: "Test Foo|Bar") |}];
+  ignore (peek_str b ~len:4);
+  [%expect {|
+    Requested 4. Available 4
+    Peek: "|Bar" |}]
