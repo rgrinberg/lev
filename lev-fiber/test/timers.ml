e442cb7c4fe2b4036ba6d18fc9ee8fb3f8000a17
Rudi Grinberg <me@rgrinberg.com>
2021-12-03 13:25:34 -0700

    test(lev_fiber): cancellation test
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lev-fiber/test/timers.ml
+++ lev-fiber/test/timers.ml
@@ -24,3 +24,23 @@ let%expect_test "timer wheel start/stop" =
   [%expect {|
     wheel: run
     wheel: stop |}]
+
+let%expect_test "timer wheel cancellation" =
+  Lev_fiber.run (Lev.Loop.create ()) ~f:(fun () ->
+      let* wheel = Wheel.create ~delay:10. in
+      Fiber.fork_and_join_unit
+        (fun () ->
+          let* task = Wheel.task wheel in
+          let* () = Wheel.cancel task in
+          let* result = Wheel.await task in
+          match result with
+          | `Ok -> assert false
+          | `Cancelled ->
+              print_endline "cancellation succeeded";
+              Wheel.stop wheel)
+        (fun () ->
+          print_endline "wheel: stop";
+          Wheel.run wheel));
+  [%expect {|
+    cancellation succeeded
+    wheel: stop |}]
