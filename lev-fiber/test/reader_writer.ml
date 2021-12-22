fafa88b9b7ddbdf150cd919592a3bfce59acedd6
Rudi Grinberg <me@rgrinberg.com>
2021-12-22 13:57:07 -0700

    test: blocking readers/writers
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lev-fiber/test/reader_writer.ml
+++ lev-fiber/test/reader_writer.ml
@@ -58,3 +58,50 @@ let%expect_test "pipe" =
   [%expect {|
     writer: finished
     reader: foobar |}]
+
+let%expect_test "blocking pipe" =
+  let fdr, fdw = Unix.pipe ~cloexec:true () in
+  let run () =
+    let* r = Io.create fdr `Blocking Input in
+    let* w = Io.create fdw `Blocking Output in
+    let writer () =
+      let+ () =
+        Io.with_write w ~f:(fun writer ->
+            let src = "foo bar baz" in
+            let len = String.length src in
+            let dst, { Io.Slice.pos = dst_pos; len = _ } =
+              Io.Writer.prepare writer ~len
+            in
+            Bytes.blit_string ~src ~src_pos:0 ~dst ~dst_pos ~len;
+            Io.Writer.commit writer ~len;
+            Io.Writer.flush writer)
+      in
+      printfn "writer: finished";
+      Io.close w
+    in
+    let reader () =
+      let+ read =
+        Io.with_read r ~f:(fun reader ->
+            let rec loop buf =
+              match Io.Reader.available reader with
+              | `Eof -> Fiber.return (Buffer.contents buf)
+              | `Ok 0 ->
+                  let* () = Io.Reader.refill reader in
+                  loop buf
+              | `Ok _ ->
+                  let bytes, { Io.Slice.pos; len } = Io.Reader.buffer reader in
+                  Buffer.add_subbytes buf bytes pos len;
+                  Io.Reader.consume reader ~len;
+                  loop buf
+            in
+            loop (Buffer.create 12))
+      in
+      printfn "read: %S" read;
+      Io.close r
+    in
+    Fiber.fork_and_join_unit reader writer
+  in
+  Lev_fiber.run (Lev.Loop.create ()) ~f:run;
+  [%expect {|
+    writer: finished
+    read: "foo bar baz" |}]
