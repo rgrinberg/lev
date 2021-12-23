0bbac77732e2ab0a8291cbcc2a005cf5de161ab8
Rudi Grinberg <me@rgrinberg.com>
2021-12-22 19:33:47 -0700

    test: task cancellation
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lev-fiber/test/lev_fiber_threads.ml
+++ lev-fiber/test/lev_fiber_threads.ml
@@ -11,3 +11,28 @@ let%expect_test "create thread" =
   in
   run (Lev.Loop.create ()) ~f;
   [%expect {| in thread |}]
+
+let%expect_test "cancellation" =
+  let f () =
+    let* thread = Thread.create () in
+    let keep_running = Atomic.make false in
+    let* task =
+      Thread.task thread ~f:(fun () ->
+          while Atomic.get keep_running do
+            ()
+          done)
+    in
+    let* to_cancel = Thread.task thread ~f:(fun () -> assert false) in
+    let* () = Thread.cancel to_cancel in
+    let* res = Thread.await to_cancel in
+    (match res with
+    | Error `Cancelled ->
+        printfn "Successful cancellation";
+        Atomic.set keep_running false
+    | Ok _ | Error (`Exn _) -> assert false);
+    let+ res = Thread.await task in
+    (match res with Ok () -> () | Error _ -> assert false);
+    Thread.close thread
+  in
+  run (Lev.Loop.create ()) ~f;
+  [%expect {| Successful cancellation |}]
