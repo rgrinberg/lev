33c56db18975dd7f4ef92b501eef42746106f13f
Rudi Grinberg <me@rgrinberg.com>
2021-12-23 01:20:56 -0700

    test: buffer resize
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lev-fiber/test/reader_writer.ml
+++ lev-fiber/test/reader_writer.ml
@@ -33,6 +33,40 @@ let%expect_test "pipe" =
     writer: finished
     read: "foobar" |}]
 
+let%expect_test "write with resize" =
+  let run () =
+    let* input, output = Io.pipe ~cloexec:true () in
+    let len = 6120 in
+    let write () =
+      let+ () =
+        Io.with_write output ~f:(fun writer ->
+            Io.Writer.add_string writer (String.make len '1');
+            Io.Writer.flush writer)
+      in
+      Io.close output;
+      printfn "writer: finished"
+    in
+    let read () =
+      let+ contents = Io.with_read input ~f:Io.Reader.to_string in
+      let len' = String.length contents in
+      printfn "read: %d length" len';
+      assert (len = len');
+      Io.close input
+    in
+    Fiber.fork_and_join_unit read write
+  in
+  let print_errors f () =
+    Fiber.with_error_handler
+      ~on_error:(fun exn ->
+        Format.eprintf "%a@." Exn_with_backtrace.pp_uncaught exn;
+        Exn_with_backtrace.reraise exn)
+      f
+  in
+  Lev_fiber.run (Lev.Loop.create ()) ~f:(print_errors run);
+  [%expect {|
+    writer: finished
+    read: 6120 length |}]
+
 let%expect_test "blocking pipe" =
   let fdr, fdw = Unix.pipe ~cloexec:true () in
   let run () =
