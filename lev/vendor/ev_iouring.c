feea825a2f6a89661241d482607755d11d55c36a
Rudi Grinberg <me@rgrinberg.com>
2022-12-24 23:41:07 -0600

    chore(lev): update libev (#88)
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>

--- lev/vendor/ev_iouring.c
+++ lev/vendor/ev_iouring.c
@@ -348,7 +348,7 @@ iouring_tfd_cb (EV_P_ struct ev_io *w, int revents)
 
 /* called for full and partial cleanup */
 ecb_cold
-static int
+static void
 iouring_internal_destroy (EV_P)
 {
   close (iouring_tfd);
